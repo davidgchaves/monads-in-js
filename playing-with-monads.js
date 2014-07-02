0218b2e596f7c11bb0a5bee8ca3ee7d68cc2a5dd
David G Chaves <davidgchaves@gmail.com>
2014-07-02 18:25:15 +0200

    'sine :: Number -> Number' and 'cube: Number -> Number' pure functions

---
+++ playing-with-monads.js
@@ -0,0 +1,8 @@
+// 01 - sine and cube are pure functions
+
+// sine :: Number -> Number
+var sine = function(x) { return Math.sin(x); }
+
+// cube :: Number -> Number
+var cube = function(x) { return x*x*x; }
+
