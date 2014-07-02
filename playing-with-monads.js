1575c0faea558d720c2a8230df78eacae1925986
David G Chaves <davidgchaves@gmail.com>
2014-07-02 18:47:13 +0200

    'sineLog :: Number -> (Number, String)' and 'cubeLog :: Number -> (Number, String)'

--- playing-with-monads.js
+++ playing-with-monads.js
@@ -1,10 +1,10 @@
 // 01 - sine and cube are pure functions
 
 // sine :: Number -> Number
-var sine = function(x) { return Math.sin(x); }
+var sine = function(x) { return Math.sin(x); };
 
 // cube :: Number -> Number
-var cube = function(x) { return x*x*x; }
+var cube = function(x) { return x*x*x; };
 
 
 // 02 - manually composing sine and cube
@@ -20,3 +20,13 @@ var compose = function(f, g) {
 
 var sineOfCube = compose(sine, cube);
 sineOfCube(3);  // -> 0.956375928404503
+
+
+// 04 - sineLog and cubeLog are pure functions
+
+// sineLog :: Number -> (Number, String)
+var sineLog = function(x) { return [Math.sin(x), "sineLog was called."]; };
+
+// cubeLog :: Number -> (Number, String)
+var cubeLog = function(x) { return [x*x*x, "cubeLog was called."]; };
+
